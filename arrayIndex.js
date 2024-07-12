70206b60d099557f217f31008ddcb01bd9ea6277
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2024-07-12 22:53:26 +0100

    access array data with index

---
+++ arrayIndex.js
@@ -0,0 +1,11 @@
+// Access array data with index
+const array = [20, 30, 40, 50];
+// console.log(array[3]);
+
+// Loop through the array using forEach
+// array.forEach((el) => console.log(el));
+
+// Create a variable call myData and set it equal to the first value of myArray using bracket notation.
+const myArray = [59, 40, 33, 30];
+const myData = myArray[0];
+console.log(myData);

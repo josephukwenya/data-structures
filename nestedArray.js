592e63c50e1f229c45659f0de96fcec5263a1435
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2024-07-13 23:34:20 +0100

    accessing nested array

---
+++ nestedArray.js
@@ -0,0 +1,16 @@
+// Using dot and bracket notation, set the variable secondTree
+// to the second item in the trees list from myPlants
+
+const myPlants = [
+  {
+    type: "flowers",
+    list: ["rose", "tulip", "dandelion"],
+  },
+  {
+    type: "trees",
+    list: ["fir", "pine", "birch"],
+  },
+];
+
+const secondTree = myPlants[1].list[1];
+console.log(secondTree);
